f116cb318dbc8c4598bcae183b0a4a84a3fb886a
Update9 <97268451+Update9@users.noreply.github.com>
2022-03-24 06:51:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
+    BOT_TOKEN = os.environ.get("5103049162:AAE49wuhia1kV7X81wjujdVLbrMOGWw6BxM ","")
+    HEROKU_API_KEY = os.environ.get("18897684","")
     AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
+    HEROKU_APP_NAME = os.environ.get("https://qiioq.herokuapp.com/","")
